f7cf7f038be5cbc7fe49b375cfb7d9aae759a5c9
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-03-02 10:28:22 -1000

    basic server running.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-let test = 'testing server'
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// define server and client ports
+// used for cors and local port declaration
+const serverDevPort = 4741
+const clientDevPort = 7165
+const redesignDevPort = 3000
+
+const app = express()
+
+const port = process.env.PORT || serverDevPort 
+
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
