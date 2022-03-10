448357047a014394ba139f1bf730a3800ffb4e9b
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-03-10 11:08:11 -1000

    adding origin.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express()
 
 //cors 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://matthewhiggins415.github.io/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://matthewhiggins415.github.io/kakeibofrontend"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
